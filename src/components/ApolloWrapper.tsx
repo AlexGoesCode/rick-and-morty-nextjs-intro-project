ca28cd625115aa3885f638f9cf65fea910d70e9f
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-07-18 15:04:50 +0200

    Update apolloWrapper

--- src/components/ApolloWrapper.tsx
+++ src/components/ApolloWrapper.tsx
@@ -1,26 +1,23 @@
-"use client";
+'use client';
 
-import {
-  ApolloLink,
-  HttpLink,
-} from "@apollo/client";
+import { ApolloLink, HttpLink } from '@apollo/client';
 import {
   ApolloNextAppProvider,
   NextSSRApolloClient,
   NextSSRInMemoryCache,
   SSRMultipartLink,
-} from "@apollo/experimental-nextjs-app-support/ssr";
+} from '@apollo/experimental-nextjs-app-support/ssr';
 
 function makeClient() {
   const httpLink = new HttpLink({
-      // https://studio.apollographql.com/public/spacex-l4uc6p/
-      uri: "https://countries.trevorblades.com/graphql",
+    // https://studio.apollographql.com/public/spacex-l4uc6p/
+    uri: 'https://countries.trevorblades.com',
   });
 
   return new NextSSRApolloClient({
     cache: new NextSSRInMemoryCache(),
     link:
-      typeof window === "undefined"
+      typeof window === 'undefined'
         ? ApolloLink.from([
             new SSRMultipartLink({
               stripDefer: true,
